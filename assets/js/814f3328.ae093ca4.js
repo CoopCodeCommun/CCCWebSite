da2a6fe1a9f2cc26cf6761c17added3e63769fb1
Jonas Legion <jturbeaux@pm.me>
2023-10-10 13:11:53 +0200

    Deploy website - based on 77297a048faaccc547cb56cc2e335b89dd8e4f29

---
+++ assets/js/814f3328.ae093ca4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcc_cwebsite=self.webpackChunkcc_cwebsite||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Premi\xe8re note de blog !","permalink":"/blog/prems"}]}')}}]);
