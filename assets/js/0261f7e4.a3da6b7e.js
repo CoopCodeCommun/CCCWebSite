be10bb56c4636ec64a1168892c68f8bbd1d14030
Jonas Legion <jturbeaux@pm.me>
2023-10-14 19:47:08 +0200

    Deploy website - based on c944c3dc5b5383062694f6b4bdba030ede4eb4a2

---
+++ assets/js/0261f7e4.a3da6b7e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcc_cwebsite=self.webpackChunkcc_cwebsite||[]).push([[4056],{9038:e=>{e.exports=JSON.parse('{"label":"coop\xe9rative","permalink":"/docs/tags/cooperative","allTagsPath":"/docs/tags","count":1,"items":[{"id":"Services/valeurs","title":"Charte et valeur","description":"Nos engagements et nos valeurs.","permalink":"/docs/Services/valeur"}]}')}}]);
