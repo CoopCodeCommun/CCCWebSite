254a9cf7451365095234267215adf5e2eb38f4bd
Jonas Legion <jturbeaux@pm.me>
2023-10-15 12:51:35 +0200

    punchline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ function MigrationAnnouncement() {
             data-theme="dark">
             <div className={styles.announcementInner}>
                 <Translate>
-                    {`Logiciels libres, communs productifs, organisations coopératives, outils d'émancipations collectifs : La coopérative Code Commun fabrique, expérimente, recherche. Vous voulez participer ?`}
+                    {`Logiciels libres, formations, communs productifs, organisations coopératives, outils d'émancipations collectifs : La SCIC Code Commun fabrique, expérimente, recherche. Vous voulez participer ?`}
                 </Translate>
             </div>
         </div>
